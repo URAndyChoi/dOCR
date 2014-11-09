5ab63c8d9b5abc8e0ca9bbadf47f3071aefb9297
URAndyChoi <andy.choi@richmond.edu>
2014-11-09 12:26:05 -0500

    Version after refactoring dOCR

--- src/com/ramHacks/docr/DocrActivity.java
+++ src/com/ramHacks/docr/DocrActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class DocrActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
